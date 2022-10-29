002fc5844f4e2b90938a6f6e842368bf99fcff2f
zahrakhadijha <zahra.khan1@icloud.com>
2022-10-29 17:05:14 -0400

    declare dom variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById("progress");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
