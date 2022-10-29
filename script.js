5dfaa94c533e258f7694707615138ce36d391444
zahrakhadijha <zahra.khan1@icloud.com>
2022-10-29 17:16:25 -0400

    Add eventlisteners on next and previous buttons

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const progress = document.getElementById("progress");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+// EVENT LISTENERS
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  console.log(currentActive);
+});
+
+previous.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  console.log(currentActive);
+});
